ae7eba0143ec10b7f8a28f79cf7ff2ebf94472cd
JC <ezjcd101@gmail.com>
2022-12-05 04:54:38 +0800

    edited medcert

--- screens/medcert.js
+++ screens/medcert.js
@@ -5,13 +5,13 @@ const MedCert = ( {navigation} ) => {
     const [isLoading, setLoading] = useState(true);
     const [data, setData] = useState([]);
 
-    let parameter = global.fname
+    let parameter = global.id
 
     const getMed = async () => {
         try {
-        const response = await fetch(`http://10.0.2.2:8000/api/medi/${parameter}`);
+        const response = await fetch(`http://10.0.2.2:8000/api/find-medcert/${parameter}`);
         const json = await response.json();
-        setData(json.reqmed);
+        setData(json.medrec);
         } catch (error) {
         console.error(error);
         } finally {
